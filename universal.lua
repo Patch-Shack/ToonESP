5f457dd9c24135989cd6d84488cafbea08907ada
Patch-Shack <60629553+Patch-Shack@users.noreply.github.com>
2021-03-25 09:27:04 -0400

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,11 +1,17 @@
 local Lib = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Patch-Shack/ToonESP/main/UiLib.lua"))();
 local Main = Lib:new("MAIN")
-local ESP = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Patch-Shack/ToonESP/main/lib2.lua"))();
+local ESP = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Patch-Shack/ToonESP/main/lib3.lua"))();
 ESP.SetupPlayers({255, 255, 255}, {255, 255, 255})
 Main:newBtn("ESP", function(bool)
-	ToggleESP(bool)
+	ESP.ToggleESP(bool)
 end, false)
 Main:newBtn("Team Check", function(bool)
-	ToggleTeamCheck(bool)
+	ESP.ToggleTeamCheck(bool)
+end, false)
+Main:newBtn("Team Name", function(bool)
+	ESP.ToggleTeamName(bool)
+end, false)
+Main:newBtn("Distance", function(bool)
+	ESP.ToggleDistance(bool)
 end, false)
 Lib:SetCategory(Main)
