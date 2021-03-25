951bdbc22308b8bdc03e850ceb547db1dac886f4
Patch-Shack <60629553+Patch-Shack@users.noreply.github.com>
2021-03-25 13:18:18 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,21 +6,25 @@ local function TNotify(t)
 end
 
 local function loadstr(url)
-   loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Patch-Shack/ToonESP/main/examples/" .. url .. ".lua"))();
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Patch-Shack/ToonESP/main/examples/" .. url .. ".lua"))();
 end
 
 local function UniversalScript()
-   loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Patch-Shack/ToonESP/main/universal.lua"))();
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Patch-Shack/ToonESP/main/universal.lua"))();
 end
 
 local Games = {
-  ["Identity Fraud"] = 338521019,
+	["Identity Fraud"] = 338521019,
+	["Identity Fraud 2"] = 495415914,
 }
 
 if game.PlaceId == Games["Identity Fraud"] then
-  loadstr("identityfraud")
-  TNotify("Identity Fraud")
+	loadstr("identityfraud")
+	TNotify("Identity Fraud")
+elseif game.PlaceId == Games["Identity Fraud 2"] then
+	loadstr("identityfraud2")
+	TNotify("Identity Fraud 2")
 else
-  UniversalScript()
-  TNotify("Universal")
+	UniversalScript()
+	TNotify("Universal")
 end
